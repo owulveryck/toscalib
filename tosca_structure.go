ce57ed61513991d1d678fc588b0f005ebc53cc6b
Olivier W <olivier.wulveryck@gmail.com>
2015-05-31 12:38:09 +0200

    Added a test structure

--- tosca_structure.go
+++ tosca_structure.go
@@ -1,72 +1,7 @@
-package main
+package gotosca
 
-import (
-	"fmt"
-	"gopkg.in/yaml.v2"
-	"log"
-)
-
-var data = `
-tosca_definitions_version: tosca_simple_yaml_1_0_0
-
-description: Template for deploying a single server with predefined properties.
-
-topology_template:
-  inputs:
-    cpus:
-      type: integer
-      description: Number of CPUs for the server.
-      constraints:
-         - valid_values: [ 1, 2, 4, 8 ]
-  node_templates:
-    my_server:
-      type: tosca.nodes.Compute
-      capabilities:
-        # Host container properties
-        host:
-         properties:
-           num_cpus: 1
-           disk_size: 10 GB
-           mem_size: 4 MB
-        # Guest Operating System properties
-        os:
-          properties:
-            # host Operating System image properties
-            architecture: x86_64
-            type: linux 
-            distribution: rhel 
-            version: 6.5  
-    my_server2:
-      type: tosca.nodes.Compute
-      capabilities:
-        # Host container properties
-        host:
-         properties:
-           num_cpus: 1
-           disk_size: 10 GB
-           mem_size: 4 MB
-        # Guest Operating System properties
-        os:
-          properties:
-            # host Operating System image properties
-            architecture: x86_64
-            type: linux 
-            distribution: rhel 
-            version: 6.5  
-    mysql:
-      type: tosca.nodes.DBMS.MySQL
-      properties:
-        root_password: { get_input: my_mysql_rootpw }
-        port: { get_input: my_mysql_port }
-      requirements:
-        - host: db_server
-  outputs:
-    server_ip:
-      description: The private IP address of the provisioned server.  
-      value: { get_attribute : [ my_server, private_address ] }
-`
-
-//go:generate stringer -type=TopologyTemplateStruct
+// TopologyStructure as defined in
+//http://docs.oasis-open.org/tosca/TOSCA-Simple-Profile-YAML/v1.0/csd03/TOSCA-Simple-Profile-YAML-v1.0-csd03.html#_Toc419746110
 type TopologyTemplateStruct struct {
 	ToscaDefinitionsVersion string `yaml:"tosca_definitions_version"`
 	Description             string `yaml:"description"`
@@ -88,22 +23,4 @@ type TopologyTemplateStruct struct {
 			Description string      `yaml:"description"`
 		} `yaml:"outputs,omitempty"`
 	} `yaml:"topology_template"`
-}
-
-func main() {
-	t := TopologyTemplateStruct{}
-
-	err := yaml.Unmarshal([]byte(data), &t)
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-	fmt.Printf("--- Result of the marshal:\n%v\n\n", t)
-	fmt.Printf("NodeTemplates: %v\n", t.TopologyTemplate.NodeTemplates)
-	fmt.Printf("my_server: %v\n", t.TopologyTemplate.NodeTemplates)
-
-	d, err := yaml.Marshal(&t)
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-	fmt.Printf("--- t dump:\n%s\n\n", string(d))
 }
---
+++ tosca_structure_test.go
@@ -0,0 +1,139 @@
+package gotosca
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"log"
+)
+
+var data = `
+tosca_definitions_version: tosca_simple_yaml_1_0_0
+
+description: Template for deploying a single server with predefined properties.
+
+topology_template:
+  inputs:
+    cpus:
+      type: integer
+      description: Number of CPUs for the server.
+      constraints:
+         - valid_values: [ 1, 2, 4, 8 ]
+  node_templates:
+    my_server:
+      type: tosca.nodes.Compute
+      capabilities:
+        # Host container properties
+        host:
+         properties:
+           num_cpus: 1
+           disk_size: 10 GB
+           mem_size: 4 MB
+        # Guest Operating System properties
+        os:
+          properties:
+            # host Operating System image properties
+            architecture: x86_64
+            type: linux 
+            distribution: rhel 
+            version: 6.5  
+    my_server2:
+      type: tosca.nodes.Compute
+      capabilities:
+        # Host container properties
+        host:
+         properties:
+           num_cpus: 1
+           disk_size: 10 GB
+           mem_size: 4 MB
+        # Guest Operating System properties
+        os:
+          properties:
+            # host Operating System image properties
+            architecture: x86_64
+            type: linux 
+            distribution: rhel 
+            version: 6.5  
+    mysql:
+      type: tosca.nodes.DBMS.MySQL
+      properties:
+        root_password: { get_input: my_mysql_rootpw }
+        port: { get_input: my_mysql_port }
+      requirements:
+        - host: db_server
+  outputs:
+    server_ip:
+      description: The private IP address of the provisioned server.  
+      value: { get_attribute : [ my_server, private_address ] }
+`
+
+func ExampleTopologyTemplateStruct() {
+	t := TopologyTemplateStruct{}
+
+	err := yaml.Unmarshal([]byte(data), &t)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	//fmt.Printf("--- Result of the marshal:\n%v\n\n", t)
+
+	d, err := yaml.Marshal(&t)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("%s\n\n", string(d))
+	//tosca_definitions_version: tosca_simple_yaml_1_0_0
+	//description: Template for deploying a single server with predefined properties.
+	//topology_template:
+	//  inputs:
+	//    cpus:
+	//      type: integer
+	//      description: Number of CPUs for the server.
+	//      constraints:
+	//      - valid_values:
+	//        - 1
+	//        - 2
+	//        - 4
+	//        - 8
+	//  node_templates:
+	//    my_server:
+	//      type: tosca.nodes.Compute
+	//      capabilities:
+	//        host:
+	//          properties:
+	//            disk_size: 10 GB
+	//            mem_size: 4 MB
+	//            num_cpus: "1"
+	//        os:
+	//          properties:
+	//            architecture: x86_64
+	//            distribution: rhel
+	//            type: linux
+	//            version: "6.5"
+	//    my_server2:
+	//      type: tosca.nodes.Compute
+	//      capabilities:
+	//        host:
+	//          properties:
+	//            disk_size: 10 GB
+	//            mem_size: 4 MB
+	//            num_cpus: "1"
+	//        os:
+	//          properties:
+	//            architecture: x86_64
+	//            distribution: rhel
+	//            type: linux
+	//            version: "6.5"
+	//    mysql:
+	//      type: tosca.nodes.DBMS.MySQL
+	//      properties:
+	//        port:
+	//          get_input: my_mysql_port
+	//        root_password:
+	//          get_input: my_mysql_rootpw
+	//  outputs:
+	//    server_ip:
+	//      value:
+	//        get_attribute:
+	//        - my_server
+	//        - private_address
+	//      description: The private IP address of the provisioned server.
+}
