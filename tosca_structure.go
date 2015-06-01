6bae3c03e1c889b17661b3a3b9aaa0abc12df9db
Olivier Wulveryck <olivier.wulveryck-ext@socgen.com>
2015-06-01 14:08:02 +0200

    Changed the structure

--- tosca_structure.go
+++ tosca_structure.go
@@ -1,7 +1,7 @@
 package gotosca
 
 // Type input corresponds to  `yaml:"inputs,omitempty"`
-type ToscaInputs map[string]struct {
+type ToscaInput struct {
 	Type             string      `yaml:"type"`
 	Description      string      `yaml:"description,omitempty"` // Not required
 	Constraints      interface{} `yaml:"constraints,omitempty"`
@@ -9,17 +9,24 @@ type ToscaInputs map[string]struct {
 	Occurrences      interface{} `yaml:"occurrences,omitempty"`
 }
 
+type ToscaNodeCapability struct { // A 6.1
+	Type             string            `yaml:"type"`
+	ValidSourceTypes []string          `yaml:"valid_source_types,omitempty"`
+	Properties       map[string]string `yaml:"properties,omitempty"`
+	Occurrences      interface{}       `yaml:"occurrences,omitempty"`
+}
+
 // Correspond to `yaml:"node_templates"`
-type ToscaNodeTemplates map[string]struct {
-	NodeType     string                 `yaml:"type"`
-	Properties   map[string]interface{} `yaml:"properties,omitempty"`
-	Capabilities map[string]struct {    // A 6.1
-		Type             string            `yaml:"type"`
-		ValidSourceTypes []string          `yaml:"valid_source_types,omitempty"`
-		Attributes       map[string]string `yaml:"attributes,omitempty"`
-		Properties       map[string]string `yaml:"properties,omitempty"`
-		Occurrences      interface{}       `yaml:"occurrences,omitempty"`
-	} `yaml:"capabilities,omitempty"`
+type ToscaNodeTemplate struct {
+	NodeType     string                         `yaml:"type"`
+	Properties   map[string]interface{}         `yaml:"properties,omitempty"`
+	Attributes   map[string]string              `yaml:"attributes,omitempty"`
+	Capabilities map[string]ToscaNodeCapability `yaml:"capabilities,omitempty"`
+}
+
+type ToscaOutput struct {
+	Value       interface{} `yaml:"value"`
+	Description string      `yaml:"description"`
 }
 
 // TopologyStructure as defined in
@@ -30,11 +37,8 @@ type TopologyTemplateStruct struct {
 	DlsDefinitions          struct {
 	} `yaml:"dsl_definitions"` // 15 Using YAML Macros to simplify templates
 	TopologyTemplate struct {
-		Inputs        ToscaInputs        `yaml:"inputs,omitempty"`
-		NodeTemplates ToscaNodeTemplates `yaml:"node_templates"`
-		Outputs       map[string]struct {
-			Value       interface{} `yaml:"value"`
-			Description string      `yaml:"description"`
-		} `yaml:"outputs,omitempty"`
+		Inputs        map[string]ToscaInput        `yaml:"inputs,omitempty"`
+		NodeTemplates map[string]ToscaNodeTemplate `yaml:"node_templates"`
+		Outputs       map[string]ToscaOutput       `yaml:"outputs,omitempty"`
 	} `yaml:"topology_template"`
 }
