f058130f66ba73604970ac266633304d64232d58
Olivier Wulveryck <olivier.wulveryck-ext@socgen.com>
2015-06-01 16:02:42 +0200

    Small Adaptationswq

--- tosca_structure_test.go
+++ tosca_structure_test.go
@@ -3,6 +3,7 @@ package gotosca
 import (
 	"fmt"
 	"gopkg.in/yaml.v2"
+	"reflect"
 	"testing"
 )
 
@@ -10,7 +11,8 @@ import (
 var data = `
 tosca_definitions_version: tosca_simple_yaml_1_0_0
 
-description: Template for deploying a single server with predefined properties.
+description: >
+    Template for deploying a single server with predefined properties.
 
 topology_template:
   inputs:
@@ -65,6 +67,9 @@ topology_template:
         port: { get_input: my_mysql_port }
       requirements:
         - host: db_server
+        - attachment:
+            node: my_storage
+            relationship : MyAttachesTo
   outputs:
     server_ip:
       description: The private IP address of the provisioned server.  
@@ -98,9 +103,20 @@ func TestToscaStructure(t *testing.T) {
 	}
 
 	// Play with the structure
-	for key, value := range topology.TopologyTemplate.Inputs {
-		fmt.Printf("Input: %v\n", key)
-		fmt.Printf("\tType: %v\n", value.Type)
-		fmt.Printf("\tDescription: %v\n", value.Description)
+	fmt.Printf("Type of TopologyTemplate: %v\n", reflect.TypeOf(topology.TopologyTemplate).Kind())
+	for key, nodeTemplate := range topology.TopologyTemplate.NodeTemplates {
+		fmt.Printf("=> NodeTemplateName(%v): %v\n", reflect.TypeOf(key).Kind(), key)
+		fmt.Printf("==> NodeType (%v): %v\n", reflect.TypeOf(nodeTemplate.NodeType), nodeTemplate.NodeType)
+		if nodeTemplate.Requirements != nil {
+			fmt.Printf("===> Requirements type: %v\n", reflect.TypeOf(nodeTemplate.Requirements).Kind())
+			/*
+				for i, v := range nodeTemplate.Requirements {
+				    fmt.Printf("====> %v: %v\n",i,v)
+				}
+			*/
+		}
+		for prop, propValue := range nodeTemplate.Properties {
+			fmt.Printf("===> Properties %v: %v (%v)\n", prop, propValue, reflect.TypeOf(propValue).Kind())
+		}
 	}
 }
