7686dc0fa7803cd599fec36b71a13fbb502665e4
Olivier Wulveryck <olivier.wulveryck-ext@socgen.com>
2015-06-11 17:30:54 +0200

    Work in progress

--- parser.go
+++ parser.go
@@ -9,15 +9,21 @@ import (
 // Parse a TOSCA document and fill in the structure
 // If the structure already contains data, the new data are append to the structure
 func (toscaStructure *ToscaDefinition) Parse(r io.Reader) error {
-	var tempStruct ToscaDefinition
+	var tempStruct interface{}
 	data, err := ioutil.ReadAll(r)
 	if err != nil {
 		return err
 	}
+
+	// Unmarshal the data in an interface
 	err = yaml.Unmarshal(data, &tempStruct)
 	if err != nil {
 		return err
 	}
+	// Check the interface type (should be a map)
+	// Otherwise, it may not be a tosca file
+
+
 	toscaStructure = &tempStruct
 	return nil
 
