6e75c117632667d052c79ba0dc266b5a3bea16a7
Olivier W <olivier.wulveryck@gmail.com>
2015-06-12 22:01:50 +0200

    Added the error output for the standard lifecycle interface

--- lifecycle.go
+++ lifecycle.go
@@ -2,9 +2,9 @@ package toscalib
 
 // ToscaInterfacesNodeLifecycleStandard is a go interface for the standard normative lifecycle
 type ToscaInterfacesNodeLifecycleStandard interface {
-	Create()    // description: Standard lifecycle create operation.
-	Configure() // description: Standard lifecycle configure operation.
-	Start()     // description: Standard lifecycle start operation.
-	Stop()      // description: Standard lifecycle stop operation.
-	Delete()    //description: Standard lifecycle delete operation.
+	Create() error    // description: Standard lifecycle create operation.
+	Configure() error // description: Standard lifecycle configure operation.
+	Start() error     // description: Standard lifecycle start operation.
+	Stop() error      // description: Standard lifecycle stop operation.
+	Delete() error    //description: Standard lifecycle delete operation.
 }
