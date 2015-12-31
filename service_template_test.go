4e99762a9657df17f1ffb2705c77f2e3010b25cf
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-31 13:37:17 +0000

    [WIP] No more tests for scalar value by now

--- service_template_test.go
+++ service_template_test.go
@@ -27,27 +27,4 @@ func TestParse(t *testing.T) {
 	}
 }
 
-func TestScalar(t *testing.T) {
-	var tests []Scalar
-	// Those test should be ok
-	tests = []Scalar{"1022.4 h", "1420 s", "12.4 MiB", "0.5 h", "5 Hz"}
-	for _, test := range tests {
-		val, err := test.Evaluate()
-		if err != nil {
-			t.Errorf("error: %v", err)
-		}
-		t.Logf("Scalar value for %v is %v", test, val)
-	}
-	// Those tests should be ko
-	tests = []Scalar{"1022", "qdfsf s", "12.4 G", "1 0.5 h"}
-	for _, test := range tests {
-		val, err := test.Evaluate()
-		if err == nil {
-			t.Errorf("error: %v", err)
-		}
-		t.Logf("Scalar value for %v is %v", test, val)
-	}
-
-}
-
 func TestEvaluate(t *testing.T) {}
