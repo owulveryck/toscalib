199c2cc54ca6827073d58d22c91fca3cf4ff7724
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-31 13:57:33 +0000

    [Enhancement] The execution plan is a "playbook"

--- execution_plan.go
+++ execution_plan.go
@@ -1,12 +1,31 @@
 package toscalib
 
-type ExecutionPlan struct {
+type Playbook struct {
 	AdjacencyMatrix Matrix
-	Index           map[int]*NodeTemplate
+	Index           map[int]Play
+	Inputs          map[string]PropertyDefinition
+	Outputs         map[string]Output
 }
 
-func GenerateExecutionPlan(s ServiceTemplateDefinition) ExecutionPlan {
-	var e ExecutionPlan
+type Play struct {
+	NodeTemplate  *NodeTemplate
+	OperationName string
+}
 
+func GeneratePlaybook(s ServiceTemplateDefinition) Playbook {
+	var e Playbook
+	i := 0
+	index := make(map[int]Play, 0)
+	for _, node := range s.TopologyTemplate.NodeTemplates {
+		for _, intf := range node.Interfaces {
+			for op, _ := range intf.Operations {
+				index[i] = Play{&node, op}
+				i += 1
+			}
+		}
+	}
+	e.Index = index
+	e.Inputs = s.TopologyTemplate.Inputs
+	e.Outputs = s.TopologyTemplate.Outputs
 	return e
 }
