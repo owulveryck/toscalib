a40550900c98e8eb88c597295553d2cdaf5c5ffa
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-31 14:12:21 +0000

    Example fixed

--- examples/parser.go
+++ examples/parser.go
@@ -16,23 +16,13 @@ func main() {
 	}
 
 	e := toscalib.GeneratePlaybook(t)
-	log.Println(e)
 	for i, n := range e.Index {
-		log.Printf("[%v] %v:%v", i, n.NodeTemplate.Name, n.OperationName)
+		log.Printf("[%v] %v:%v -> %v %v",
+			i,
+			n.NodeTemplate.Name,
+			n.OperationName,
+			n.NodeTemplate.Interfaces[n.InterfaceName].Operations[n.OperationName].Implementation,
+			n.NodeTemplate.Interfaces[n.InterfaceName].Operations[n.OperationName].Inputs,
+		)
 	}
-	for nn, nt := range t.TopologyTemplate.NodeTemplates {
-		fmt.Println(nn)
-		for _, intf := range nt.Interfaces {
-			for opn, opp := range intf.Operations {
-				fmt.Printf("%v: %v\n", opn, opp.Implementation)
-			}
-		}
-	}
-	/*
-		o, err := yaml.Marshal(&t)
-			if err != nil {
-				log.Panic(err)
-			}
-			fmt.Println(string(o))
-	*/
 }
