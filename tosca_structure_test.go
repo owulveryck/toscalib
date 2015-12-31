6ad9c677b3aea2bd59db5a6e800900a79e8ba79f
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-31 09:13:48 +0000

    [RM] Removed the dummy unsueful tests

--- tosca_structure_test.go
+++ tosca_structure_test.go
@@ -1,74 +1,10 @@
 package toscalib
 
 import (
-	"io/ioutil"
 	"testing"
-
-	"encoding/json"
-	"gopkg.in/yaml.v2"
-	"os"
 )
 
-// teststructures does a simple mashalling/unmarshalling of some testfiles
-func TestStructures(t *testing.T) {
-	// this is a map with the structure to be tested and the corresponding example
-	examples := map[string]interface{}{
-		"testFiles/constraintsTest.yaml": ConstraintClause{},
-		"testFiles/topologyTest.yaml":    ServiceTemplateDefinition{},
-		"testFiles/propertyTest.yaml":    map[string]PropertyDefinition{},
-	}
-
-	for testFile, mystruct := range examples {
-
-		file, err := ioutil.ReadFile(testFile)
-		if err != nil {
-			t.Errorf("error: %v", err)
-		}
-		err = yaml.Unmarshal(file, &mystruct)
-		if err != nil {
-			t.Errorf("error: %v", err)
-		}
-		t.Logf("--- Result of the unmarshal:\n%v\n\n", mystruct)
-
-		d, err := yaml.Marshal(&mystruct)
-		if err != nil {
-			t.Errorf("error: %v", err)
-		}
-
-		t.Logf("%s\n\n", string(d))
-	}
-}
-
 func TestParse(t *testing.T) {
-	var toscaStructure ServiceTemplateDefinition
-	file, err := os.Open("examples/tosca_elk.yaml")
-
-	if err != nil {
-		t.Errorf("error: %v", err)
-	}
-	err = toscaStructure.Parse(file)
-	if err != nil {
-		t.Errorf("error: %v", err)
-	}
-	t.Logf("--- Result of the unmarshal:\n%v\n\n", toscaStructure)
-	d, err := yaml.Marshal(&toscaStructure)
-	if err != nil {
-		t.Errorf("error: %v", err)
-	}
-
-	t.Logf("%s\n\n", string(d))
-	// Test the json marshaling
-	if err != nil {
-		t.Errorf("error: %v", err)
-	}
-	d, err = json.MarshalIndent(toscaStructure.TopologyTemplate.NodeTemplates, "", "    ")
-	if err != nil {
-		t.Errorf("error: %v", err)
-	}
-	t.Logf("Json:%v", string(d))
-	// Test adjacencyMatrix
-	toscaStructure.DotExecutionWorkflow(ioutil.Discard)
-	toscaStructure.PrintDot(ioutil.Discard)
 }
 
 func TestScalar(t *testing.T) {
