b2f507755822563ce2eaba6888f2a7407807f403
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-31 11:01:56 +0000

    [Fix] Scalar value shoud be accurate

--- tosca_namespace_alias.go
+++ tosca_namespace_alias.go
@@ -3,11 +3,10 @@
 package toscalib
 
 import (
-	"errors"
+	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
-	"time"
 )
 
 // This implements the type defined in Appendix A 2 of the definition file
@@ -87,97 +86,36 @@ type Frequency int64
 // Scalar type as defined in Appendis 2.6.
 // The scalar unit type can be used to define scalar values along with a unit from the list of recognized units
 // Scalar type may be time.Duration, Size or Frequency
-type Scalar string
+type Scalar struct {
+	Value float64
+	Unit  string
+}
 
 // UnmarshalYAML implements the yaml.Unmarshaler interface
 // Unmarshals a string of the form "scalar unit" into a Scalar, validating that scalar and unit are valid
-func (scalar *Scalar) UnmarshalYAML(unmarshal func(interface{}) error) error {
-	var scalarString string
-	err := unmarshal(&scalarString)
+func (s *Scalar) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var sString string
+	err := unmarshal(&sString)
 	if err != nil {
 		return err
 	}
-	// Check if the scalar has two fields (one for the value, and the other one for the unit)
-	scalars := strings.Fields(scalarString)
-	if len(scalars) > 2 {
-		return errors.New("Not a TOSCA scalar")
+	// Check if the s has two fields (one for the value, and the other one for the unit)
+	ss := strings.Fields(sString)
+	if len(ss) > 2 {
+		return fmt.Errorf("Not a TOSCA scalar")
 	}
-	// Check if the scalar is a float64
-	_, err = strconv.ParseFloat(scalars[0], 64)
-	if err != nil {
-		return err
+	re := regexp.MustCompile("^([0-9.]+)[[:blank:]]*(B|kB|KiB|MB|MiB|GB|GiB|TB|TiB|d|h|m|s|ms|us|ns|Hz|kHz|MHz|GHz)$")
+	res := re.FindStringSubmatch(sString)
+	if err != nil || len(res) != 3 {
+		return fmt.Errorf("Tosca type unkown")
 	}
-	if len(scalars) == 2 {
-		// Check if a unit is known
-		res, err := regexp.MatchString("B|kB|KiB|MB|MiB|GB|GiB|TB|TiB|d|h|m|s|ms|us|ns|Hz|kHz|MHz|GHz", scalars[1])
-		if err != nil || res == false {
-			return errors.New("Tosca type unkown")
-		}
+	val, err := strconv.ParseFloat(res[1], 64)
+	if err != nil || len(res) != 3 {
+		return fmt.Errorf("Not a number", res[1])
 	}
-	*scalar = Scalar(scalarString)
+	(*s).Value = val
+	(*s).Unit = res[2]
 	return nil
-}
-
-// Evaluate returns the "go" value for scalar
-// If type is a Duration, returns a time.Duration type with the associated value
-// If type is Size, returns the size in "byte number"
-// If type is Frequency, returns the frequency in Hz (= one cycle per second)
-func (scalar *Scalar) Evaluate() (interface{}, error) {
-	// Check if the scalar has two fields (one for the value, and the other one for the unit)
-	if strings.Count(string(*scalar), " ") != 1 {
-		return nil, errors.New("Not a TOSCA scalar")
-	}
-	// Value should be numeric convert it
-	var val float64
-	val, err := strconv.ParseFloat(strings.Fields(string(*scalar))[0], 64)
-	if err != nil {
-		return nil, err
-	}
-	unit := strings.Fields(string(*scalar))[1]
-	// Size definition
-	var isSize = regexp.MustCompile("B|kB|KiB|MB|MiB|GB|GiB|TB|TiB")
-	// scalar-unit.time as described in Appendis 2.6.5
-	var isDuration = regexp.MustCompile("d|h|m|s|ms|us|ns")
-	var isFrequency = regexp.MustCompile("Hz|kHz|MHz|GHz")
-	switch {
-	case isSize.MatchString(unit):
-		var B float64 = 1
-		var unitMapSize = map[string]float64{
-			"B":   B,                 // A Byte
-			"kB":  1000 * B,          // kilobyte (1000 bytes)
-			"KiB": 2014 * B,          // kibibytes (1024 bytes)
-			"MB":  1000000 * B,       // megabyte (1000000 bytes)
-			"MiB": 1048576 * B,       // mebibyte (1048576 bytes)
-			"GB":  1000000000 * B,    // gigabyte (1000000000 bytes)
-			"GiB": 1073741824 * B,    // gibibytes (1073741824 bytes)
-			"TB":  1000000000000 * B, // terabyte (1000000000000 bytes)
-			"TiB": 1099511627776 * B, // tebibyte (1099511627776 bytes)
-		}
-		return val * unitMapSize[unit], nil
-	case isFrequency.MatchString(unit):
-		var HZ float64 = 1
-		var unitMapFrequency = map[string]float64{
-			"Hz":  HZ,              // Hertz, or Hz. equals one cycle per second.
-			"kHz": 1000 * HZ,       // Kilohertz, or kHz, equals to 1,000 Hertz
-			"MHz": 1000000 * HZ,    //    Megahertz, or MHz, equals to 1,000,000 Hertz or 1,000 kHz
-			"GHz": 1000000000 * HZ, // Gigahertz, or GHz, equals to 1,000,000,000 Hertz, or 1,000,000 kHz, or 1,000 MHz.
-		}
-		return val * unitMapFrequency[unit], nil
-	case isDuration.MatchString(unit):
-		var H = time.Hour
-		var unitMapTime = map[string]time.Duration{
-			"D":  H * 24,           //  days
-			"H":  time.Hour,        // hours
-			"M":  time.Minute,      // minutes
-			"S":  time.Second,      //  seconds
-			"Ms": time.Millisecond, //  milliseconds
-			"Us": time.Microsecond, // microseconds
-			"Ns": time.Nanosecond,  // nanoseconds
-		}
-		return time.Duration(val) * unitMapTime[unit], nil
-	default:
-		return nil, errors.New("Cannot parse scalar")
-	}
 }
 
 // Regex type used in the constraint definition (Appendix A 5.2.1)
