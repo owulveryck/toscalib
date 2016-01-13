8cb1e07e0b7af930c464c9f1a084b31504c54542
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2016-01-13 15:24:40 +0100

    Configure Realtionship lifecycle

--- toscaexec/sort_lifecycle.go
+++ toscaexec/sort_lifecycle.go
@@ -31,3 +31,29 @@ func (p Interfaces) Less(i, j int) bool {
 	// p[j].IsRequirement is true obviously
 	return !(order[p[j].Method] <= order["start"])
 }
+
+const source = false
+const node = false
+const relationship = true
+const requirement = true
+
+type operation struct {
+	node      bool
+	operation string
+}
+
+// Defines the order of execution of a relationship configuration
+var orderRelationshipConfiguration = map[operation]int{
+	operation{node, "create"}:                        0,
+	operation{relationship, "pre_configure_source"}:  1,
+	operation{relationship, "pre_configure_target"}:  2,
+	operation{node, "configure"}:                     3,
+	operation{relationship, "post_configure_source"}: 4,
+	operation{relationship, "post_configure_target"}: 5,
+	operation{node, "start"}:                         6,
+	operation{relationship, "add_target"}:            7,
+	operation{relationship, "remove_target"}:         8,
+	operation{relationship, "target_changed"}:        9,
+	operation{node, "stop"}:                          10,
+	operation{node, "delete"}:                        11,
+}
