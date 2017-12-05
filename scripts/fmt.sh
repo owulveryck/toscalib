1bc18c6a4f7938fa563913cde733ace0ab7004c9
kenjones <kenjones@cisco.com>
2017-12-05 18:35:06 -0500

    Task: resolved travis-ci issue; detached HEAD was root cause

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -2,7 +2,7 @@
 
 find . \( -path ./vendor -o -path ./.glide \) -prune -o -name "*.go" -exec goimports -w {} \;
 
-if ! git diff-index --quiet HEAD; then
+if test -n "$(git status --porcelain)"; then
     echo "goimports modified code; requires attention!"
     if [[ "${CI}" == "true" ]]; then
         exit 1
