8bcbdd8af441f8c529c6b8fae199ec8d7cfecc19
kenjones <kenjones@cisco.com>
2017-12-05 18:53:42 -0500

    Task: remove use of 'test' in if condition for dirty branch

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -2,7 +2,7 @@
 
 find . \( -path ./vendor -o -path ./.glide \) -prune -o -name "*.go" -exec goimports -w {} \;
 
-if test -n "$(git status --porcelain)"; then
+if [[ -n "$(git -c core.fileMode=false status --porcelain)" ]]; then
     echo "goimports modified code; requires attention!"
     if [[ "${CI}" == "true" ]]; then
         exit 1
